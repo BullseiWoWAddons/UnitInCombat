7ef2374cb359e7ca272e3e47a9f7be6b81b8a16c
bullsei <bullsei@users.noreply.github.com>
2022-05-26 22:57:28 +0200

    removed unecessary stuff

--- Static.lua
+++ Static.lua
@@ -1,12 +1,4 @@
 local AddonName, Data = ...
-local GetAddOnMetadata = GetAddOnMetadata
-
-local L = Data.L
-local AceConfig = LibStub("AceConfig-3.0")
-local AceConfigDialog = LibStub("AceConfigDialog-3.0")
-local AceConfigRegistry = LibStub("AceConfigRegistry-3.0")
-local LSM = LibStub("LibSharedMedia-3.0")
-
 
 Data.FramePoints = {
 	"TOPLEFT",
